529066eb138ebf2bd78aa00a7f87e4d4fdad8128
gggkamil <74270259+gggkamil@users.noreply.github.com>
2024-09-10 01:34:44 +0200

    Security Headres added

--- API/Program.cs
+++ API/Program.cs
@@ -31,10 +31,30 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
+app.UseXContentTypeOptions();
+app.UseReferrerPolicy(opt=>opt.NoReferrer());
+app.UseXXssProtection(opt=> opt.EnabledWithBlockMode());
+app.UseXfo(opt=>opt.Deny());
+app.UseCsp(opt=>opt
+    .BlockAllMixedContent()
+    .StyleSources(s=> s.Self().CustomSources("http://fonts.googleapis.com"))
+    .FontSources(s=>s.Self().CustomSources("http://fonts.gstatic.com","data:"))
+    .FormActions(s=>s.Self())
+    .FrameSources(s=>s.Self())
+    .ImageSources(s=>s.Self().CustomSources("blob:","https://res.cloudinary.com"))
+    .ScriptSources(s=>s.Self())
+ );
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+else{
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Add("Strict-Transport-Security","max-age=31536000");
+        await next.Invoke();
+    });
 }
 
 app.UseCors("CorsPolicy");
